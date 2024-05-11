6e095d329c47dd31f4e517d4aee881f6f92ead53
Guibo Li <guiboli@deepmirror.com>
2024-05-11 11:52:18 +0800

    feat: update

--- dist/platforms/windows/set_gitcredential.ps1
+++ dist/platforms/windows/set_gitcredential.ps1
@@ -9,6 +9,7 @@ else {
     git config --global credential.helper "store --file ~/.git-credentials"
     git config --global user.email $env:GIT_USER_EMAIL
     git config --global user.name $env:GIT_USER_NAME
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
     git clone https://github.com/deepmirrorinc/MobiliTest.git ~/
 }
 
